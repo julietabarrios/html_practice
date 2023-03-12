bd135915a0f912b68fedf250d1f163d5a389a2ab
Julieta Barrios <julietabarriosd@gmail.com>
2023-03-12 14:08:46 +0100

    numToString.js

---
+++ prep_work/javascript_challenges/numToString.js
@@ -0,0 +1,10 @@
+// write a function which takes one argument which is a number
+// and returns the same value as a string
+
+numberToString(10) 
+// returns
+"10" 
+
+function numberToString(num){
+    return num.toString();
+}
